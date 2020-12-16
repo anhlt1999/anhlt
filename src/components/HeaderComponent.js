3086aa696ca926d908a4d46ba835bcbdeb4d21d2
anhlt1999 <tal180899@gmail.com>
2020-12-17 02:00:55 +0700

    Update HeaderComponent.js

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -14,7 +14,7 @@ class HeaderComponent extends Component {
             <div>
                 <header>
                     <nav className="navbar navbar-expand-md navbar-dark bg-dark">
-                    <div><a href="http://localhost:3000/" className="navbar-brand"> Quản lý phòng khám tư</a></div>
+                    <div><a href="https://ltwebptit.herokuapp.com/" className="navbar-brand"> Quản lý phòng khám tư</a></div>
                     </nav>
                 </header>
             </div>
